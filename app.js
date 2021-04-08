ec73eef2bb422785565cdbeaceb152f943af28c9
Johar Ali <50805982+JoharPiaic@users.noreply.github.com>
2021-04-08 23:51:32 +0500

    Case statement

---
+++ app.js
@@ -0,0 +1,30 @@
+// var aaa = [ "Ali", "Johar","Saad"]
+// var bbb = [ "Saim", "Aman"]
+
+// for(var  a=0; a<=aaa.length; a++){
+//     document.write(aaa + " " + aaa + "<br>");
+//     for(var  b=0; b<=bbb.length; b++){
+//        document.write(bbb + " " + bbb + "<br>");
+  
+//     }
+
+// }
+
+// for( var a=1; a<=9; a++){
+//     for(var b=1; b<=a; b++){
+//     document.write(a + " ")
+//     }
+//     document.write("<br>")
+// }
+
+var names = "JOHAR"
+var userInput = prompt("Enter Name");
+var newOne = userInput.toUpperCase();
+
+if(newOne === names){
+    alert(" aLL RIGHT Sahi Ha");
+}
+else{
+    alert(" Nahi Jani Galt ha");
+}
+
